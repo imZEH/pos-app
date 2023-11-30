bd760221a9d77da88e457cdad94cbcbb20ff0054
Neil <neil@webcoda.com.au>
2023-11-30 23:13:36 +0800

    add routes for page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/buttons', function () {
+    return view('buttons');
+});
+
+Route::get('/cards', function () {
+    return view('cards');
+});
